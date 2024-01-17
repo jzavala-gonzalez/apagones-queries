dc537792ebaa7d10609857d43337c6781d3bfa8b
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-01-17 15:57:12 -0400

    Add transfer manager to boost download speed

--- scripts/utils.py
+++ scripts/utils.py
@@ -29,9 +29,15 @@ def get_all_objects(s3_client, prefix=None):
             for obj in page['Contents']:
                 yield obj
 
-def download_object(s3_client, obj_key, local_path):
+def download_object(s3_manager, obj_key, local_path, verbose=False):
     ''' Download an object from S3 to a local path '''
     file_directory = os.path.dirname(local_path)
     os.makedirs(file_directory, exist_ok=True)
-    print(f'Downloading {obj_key} to {local_path}')
-    s3_client.download_file('archiva-apagones', obj_key, local_path)
+    if verbose:
+        print(f'Downloading {obj_key} to {local_path}')
+    # s3_client.download_file('archiva-apagones', obj_key, local_path) # when using just a client
+    s3_manager.download(
+        bucket='archiva-apagones',
+        key=obj_key,
+        fileobj=local_path,
+    )
